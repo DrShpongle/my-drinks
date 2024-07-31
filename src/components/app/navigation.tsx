f49c8cc66e7adc102bbe10e5bed193ef7ad2bd5b
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-07-31 11:39:05 +0200

    add logo

--- src/components/app/navigation.tsx
+++ src/components/app/navigation.tsx
@@ -2,14 +2,18 @@ import Link from 'next/link'
 
 import { ThemeToggle } from '@/components/theme-toggle'
 import Favorites from '@/components/favorites'
+import Logo from '@/components/logo'
 
 const Navigation = () => {
   return (
     <header className="py-3 md:py-4 w-full border-b border-border fixed bg-background z-10">
       <nav className="container flex items-center justify-between">
-        <div>
-          <Link href="/" className="text-4xl md:text-5xl">
-            🍹
+        <div className="flex">
+          <Link
+            href="/"
+            className="text-4xl md:text-5xl w-36 md:w-44 text-black dark:text-white"
+          >
+            <Logo />
           </Link>
         </div>
         <div className="flex space-x-2 items-center">
